50f9e17d0a83a120389693796d54116e3fed1a02
AaronFae <AaronFae@163.com>
2018-03-16 08:43:33 +0800

    2.4 profile

---
+++ src/main/java/ex/aaronfae/ch2/profile/ProfileConfig.java
@@ -0,0 +1,32 @@
+package ex.aaronfae.ch2.profile;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class ProfileConfig {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.getEnvironment().setActiveProfiles("dev");
+        context.register(ProfileConfig.class);
+        context.refresh();
+
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        System.out.println(demoBean.getContent());
+    }
+
+    @Bean
+    @Profile("dev")
+    public DemoBean devDemoBean() {
+        return new DemoBean("from development profile");
+    }
+
+    @Bean
+    @Profile("prod")
+    public DemoBean prodDemoBean() {
+        return new DemoBean("from production profile");
+    }
+}
